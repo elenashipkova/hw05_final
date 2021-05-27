ff0341b2d15e8e1594ec93b7e426d32c0b4de14b
Elena <shipkovalena@gmail.com>
2021-05-28 02:56:22 +0300

    Проект спринта6 ревью2 тесты

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -56,7 +56,6 @@ def profile(request, username):
 
 def post_view(request, username, post_id):
     post = get_object_or_404(Post, id=post_id, author__username=username)
-    comments = post.comments.all()
     form = CommentForm()
     return render(
         request,
